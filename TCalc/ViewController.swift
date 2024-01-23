f8efd9d2bd279509b2abce8a6627a8b68ed0dcdc
Mashnin Egor <mashnin_yegor@mail.ru>
2024-01-23 22:09:07 +0300

    Initial Commit Main screen filled All buttons print their title text to consol

--- TCalc/ViewController.swift
+++ TCalc/ViewController.swift
@@ -11,9 +11,11 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func btnDidTap(_ sender: UIButton) {
+        print(sender.titleLabel?.text ?? "")
+    }
 }
 
